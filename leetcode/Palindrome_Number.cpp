86176f4860b94549960b1a0fd25675b8252c2079
jeo33 <34943011+jeo33@users.noreply.github.com>
2021-11-04 22:27:08 -0400

    Create Palindrome_Number.cpp

---
+++ leetcode/Palindrome_Number.cpp
@@ -0,0 +1,47 @@
+/*
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Example 4:
+
+Input: x = -101
+Output: false
+*/
+
+class Solution {
+        public:
+            bool isPalindrome(int x) {
+                string s = to_string(x);
+                if((x<10&&x>0))return true;
+                if (x < 0 || (x / 10 == 0 && x > 0)) return false;
+                
+                int size = s.length()-1;
+                
+                for (int i=0;i<size;i++)
+                {
+                    if (s.at(i) != s.at(size - i))
+                        return false;
+                    
+
+                }
+                return true;
+            }
+        };
